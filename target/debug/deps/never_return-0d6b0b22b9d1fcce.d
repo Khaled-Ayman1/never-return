f60faf59760f8ef4c4c4f10dbc115672b7ccf183
Khaled Ayman <khaled.ayman.salah.ahmed@gmail.com>
2024-09-15 18:43:54 +0300

    Rust Template

---
+++ target/debug/deps/never_return-0d6b0b22b9d1fcce.d
@@ -0,0 +1,6 @@
+C:\Users\user\Desktop\Home\Study\University\GP\Rust\never-return\target\debug\deps\libnever_return-0d6b0b22b9d1fcce.rmeta: src/main.rs src\solution.rs
+
+C:\Users\user\Desktop\Home\Study\University\GP\Rust\never-return\target\debug\deps\never_return-0d6b0b22b9d1fcce.d: src/main.rs src\solution.rs
+
+src/main.rs:
+src\solution.rs:
---
+++ target/debug/deps/never_return-f2df177283447f90.d
@@ -0,0 +1,6 @@
+C:\Users\user\Desktop\Home\Study\University\GP\Rust\never-return\target\debug\deps\libnever_return-f2df177283447f90.rmeta: src/main.rs src\solution.rs
+
+C:\Users\user\Desktop\Home\Study\University\GP\Rust\never-return\target\debug\deps\never_return-f2df177283447f90.d: src/main.rs src\solution.rs
+
+src/main.rs:
+src\solution.rs:
